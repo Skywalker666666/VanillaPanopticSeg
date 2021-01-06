7a52fc8f1134bacd4db32ca0f68cdbad5051dfed
skywalker666666 <zhiliu_yang@hotmail.com>
2021-01-05 23:40:33 -0500

    initial version

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='panoptic',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Implementation of https://arxiv.org/abs/1801.00868',
+    author='Yann LG',
+    license='MIT',
+)
